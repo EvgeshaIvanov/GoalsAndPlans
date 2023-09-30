452796208e15ada68eb80e71ca084e2bab5671f1
Eugene_Ivanov <99675396+EvgeshaIvanov@users.noreply.github.com>
2023-09-30 16:56:34 +0300

    [1] create new modules

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GoalsAndPlans"
+include(":app")
+include(":navigation")
+include(":greeting")
+include(":home")
+include(":stats")
